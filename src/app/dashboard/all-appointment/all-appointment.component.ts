a5bbdd18e4c31fe7709e8e6e696fd99c2c1e24c3
Subham-Bose <subhambose1993@gmail.com>
2022-08-16 05:10:31 +0530

    appointment done(subham)

--- src/app/dashboard/all-appointment/all-appointment.component.ts
+++ src/app/dashboard/all-appointment/all-appointment.component.ts
@@ -1,23 +1,57 @@
+import { formatDate } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CalenderComponent } from './calender/calender.component';
 
 @Component({
   selector: 'all-appointments',
   templateUrl: './all-appointment.component.html',
-  styleUrls: ['./all-appointment.component.scss']
+  styleUrls: ['./all-appointment.component.scss'],
 })
 export class AllAppointmentComponent implements OnInit {
   currentStatus: string = 'All';
+  startDate: string = formatDate(Date.now(), 'yyyy-MM-dd', 'en-US');
+  endDate: string = formatDate(Date.now(), 'yyyy-MM-dd', 'en-US');
+  filters: any = {
+    status: this.currentStatus,
+    startDate: formatDate(Date.now(), 'yyyy-MM-dd', 'en-US'),
+    endDate: formatDate(Date.now(), 'yyyy-MM-dd', 'en-US'),
+  };
   showGrid: boolean = true;
-  role:string='vet';
-  @Output() isGridView = new EventEmitter<boolean>();
+  role: string = 'vet';
 
-  constructor() {}
+  constructor(private router: Router, private dialog: MatDialog) {}
 
   ngOnInit(): void {}
-  SelectChange(e) {
-    this.currentStatus = e.target.value;
+
+  SelectChange(event) {
+    this.filters = {
+      status: event.target.value,
+      startDate: this.startDate,
+      endDate: this.endDate,
+    };
   }
   newAppointment() {
-    this.isGridView.emit(false);
+    this.router.navigate(['addAppointmentForDoctor']);
+  }
+  openCalenderDialog() {
+    var dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.width = '300px';
+
+    var dialogRef = this.dialog.open(CalenderComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe({
+      next: (data: any) => {
+        this.filters = {
+          status: this.currentStatus,
+          startDate: data.dates.startDate,
+          endDate: data.dates.endDate,
+        };
+        this.startDate = data.dates.startDate;
+        this.endDate = data.dates.endDate;
+      },
+    });
   }
 }
